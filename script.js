6d86faaeb04b0ae4b5e331be96cb1cc244314b5b
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-09-17 15:50:10 -0500

    added button

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", function() {
+    let button = document.createElement('button');
+    button.innerText = 'Add Square';
+    document.body.appendChild(button);
+
+
+
+
+
+        //END OF CONTAINER//
+})
+
