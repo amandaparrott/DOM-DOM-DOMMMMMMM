c428bc1392f2d5220276077e08915d1853ecafd8
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-09-17 17:45:04 -0500

    added id counter and classes

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+
+//button//
 window.addEventListener("DOMContentLoaded", function() {
+    let div = document.createElement('div');
     let button = document.createElement('button');
     button.innerText = 'Add Square';
-    document.body.appendChild(button);
+    div.appendChild(button);
+    document.body.appendChild(div);
 
+//add div when button is clicked, assign id number//
+button.addEventListener('click', function() {
+    let div = document.createElement('div');
+    squareId+1;
+    div.innerText = squareId;
+    div.id = 'square' + squareId++; 
+    div.className = 'square';
+    document.body.appendChild(div);
+});
 
-
+let squareId = 0;
 
 
         //END OF CONTAINER//
 })
 
+
+// for hover, maybe create paragraph, look up event listeners?
